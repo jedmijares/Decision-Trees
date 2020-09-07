fcacc19fa3c044d249a8b475abfeb2abb5400900
Jed <jamijares3@gmail.com>
2020-09-07 16:05:37 -0500

    make list of numbers instead of strings

--- decisionTrees.py
+++ decisionTrees.py
@@ -8,7 +8,7 @@ with open(r'data\synthetic-1.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         # print(f'\t{row[0]} \t {row[1]} \t {row[2]}.')
-        dataPoints.append((row[0], row[1], row[2]))
+        dataPoints.append((float(row[0]), float(row[1]), bool(int(row[2]))))
         line_count += 1
     print(f'Processed {line_count} lines.')
 
