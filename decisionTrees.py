a08c098b59fbb781a96c657bff01748fea572cb5
Jed <jamijares3@gmail.com>
2020-09-07 13:17:11 -0500

    places all points in a list

--- decisionTrees.py
+++ decisionTrees.py
@@ -1,11 +1,15 @@
 import csv
 
+dataPoints = []
+
+# https://realpython.com/python-csv/
 with open(r'data\synthetic-1.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        print(f'\t{row[0]} \t {row[1]} \t {row[2]}.')
+        # print(f'\t{row[0]} \t {row[1]} \t {row[2]}.')
+        dataPoints.append((row[0], row[1], row[2]))
         line_count += 1
     print(f'Processed {line_count} lines.')
 
-print("This line will be printed.")
+print(dataPoints)
