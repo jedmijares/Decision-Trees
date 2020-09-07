166c2ace88fa67a6a9fc2c094dc7b44406849bb0
Jed <jamijares3@gmail.com>
2020-09-07 16:05:51 -0500

    calculates ranges

--- decisionTrees.py
+++ decisionTrees.py
@@ -1,4 +1,5 @@
 import csv
+from operator import itemgetter
 
 dataPoints = []
 
@@ -10,6 +11,13 @@ with open(r'data\synthetic-1.csv') as csv_file:
         # print(f'\t{row[0]} \t {row[1]} \t {row[2]}.')
         dataPoints.append((float(row[0]), float(row[1]), bool(int(row[2]))))
         line_count += 1
-    print(f'Processed {line_count} lines.')
+    # print(f'Processed {line_count} lines.')
 
 print(dataPoints)
+
+# https://www.tutorialspoint.com/get-first-element-with-maximum-value-in-list-of-tuples-in-python
+range0 = max(dataPoints, key=itemgetter(0))[0] - min(dataPoints, key=itemgetter(0))[0]
+range1 = max(dataPoints, key=itemgetter(1))[1] - min(dataPoints, key=itemgetter(1))[1]
+
+print(range0)
+print(range1)
