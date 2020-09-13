66cf93d242f3685706a460da58146b55ea0f9645
Jed <jamijares3@gmail.com>
2020-09-12 20:43:45 -0500

    removes commented code

--- decisionTrees.py
+++ decisionTrees.py
@@ -9,15 +9,13 @@ import matplotlib.pyplot as plt # python -m pip install -U matplotlib
 dataPoints = []
 
 # https://realpython.com/python-csv/
-with open(r'data\synthetic-4.csv') as csv_file:
+with open(r'data\synthetic-1.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     # ncol = len(next(csv_reader)) # Read first line and count columns
     # csv_file.seek(0)              # go back to beginning of file
     for row in csv_reader:
         dataPoints.append((float(row[0]), float(row[1]), bool(int(row[2]))))
-    # print(f'Processed {line_count} lines.')
 
-# print(dataPoints)
 
 def ID3(examples, targetAttribute, availableAttributes):
     thisNode = AnyNode()
@@ -77,19 +75,14 @@ def predict(rootNode, point):
             return predict(child, point)
 
 root = ID3(dataPoints, 2, [0, 1])
-# print(RenderTree(root)) 
-# print(predict(root, (1,0)))
-# print(RenderTree(root)) 
-# print(root.children)
+print(RenderTree(root)) 
 
 x = [elem[0] for elem in dataPoints]
-x_min = min(x)
-x_max = max(x)
+x_min = min(x) - 1
+x_max = max(x) + 1
 y = [elem[1] for elem in dataPoints]
-y_min = min(y)
-y_max = max(y)
-# x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-# y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+y_min = min(y) - 1
+y_max = max(y) + 1
 plot_step = 0.2
 xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step), np.arange(y_min, y_max, plot_step))
 plt.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
@@ -100,30 +93,11 @@ for pair in np.c_[xx.ravel(), yy.ravel()]:
 Z = np.asarray(Z)
 Z = Z.reshape(xx.shape)
 
-# Z = np.array()
-# for i in range(len(xx)):
-#     Z.insert(predict(root, (xx[i], yy[i])))
-# Z = np.asarray(Z)
+plt.contourf(xx, yy, Z)
 
+x = [elem[0] for elem in dataPoints]
+y = [elem[1] for elem in dataPoints]
+colors = [elem[2] for elem in dataPoints]
 
-
-# Z = np.array([])
-# for index1 in range(xx.shape[0]):
-#     for index2 in range(xx.shape[1]):
-#         Z[index1][index2] = predict(root, (xx[index1][index2], yy[index1][index2]))
-
-    # Z = predict(root, (xx , yy[index]))
-# Z = predict(root, np.c_[xx.ravel(), yy.ravel()])
-# Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.RdYlBu)
-
-# print(xx.shape)
-# print(yy.shape)
-
-# # N = 50
-# x = [elem[0] for elem in dataPoints]
-# y = [elem[1] for elem in dataPoints]
-# colors = [elem[2] for elem in dataPoints]
-
-# plt.scatter(x, y, c = colors)
+plt.scatter(x, y, c = colors)
 plt.show()
